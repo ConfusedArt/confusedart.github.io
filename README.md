584b8f639a7991bd6a0efdedb6913457bb2f4c8b
Retr0-01 <contact@retr0.dev>
2023-11-20 21:55:43 +0200

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# confusedart.github.io
---
+++ src/util/consts.ts
@@ -0,0 +1,16 @@
+import { type Props as IconProps } from "../components/Icon.astro";
+
+export const socialLinks: { label: string; href: string; icon: IconProps["icon"] }[] =
+[
+	{
+		label: "Twitter",
+		href: "",
+		icon: "twitter-logo",
+	},
+	{
+		label: "FaceBook",
+		href: "",
+		icon: "facebook-logo",
+	},
+];
+ 
